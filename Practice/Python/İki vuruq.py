66ad264464ccdb4f0ac3e08541d2e303700bd6f5
Murad685 <orucowmurad@gmail.com>
2021-04-25 21:19:54 +0400

    pythonda bezi mesele hell edildi

---
+++ Practice/Python/İki vuruq.py
@@ -0,0 +1,6 @@
+# Hansı ən kiçik n ədədini k (1 ≤ k ≤ 50) sayda üsulla n = a · b şəklində iki vuruğun hasili kimi göstərmək olar?
+# a · b və b · a hasilləri eyni üsul sayılır, bütün ədədlər naturaldır.
+
+n=int(input())
+while (a*b):
+    if :
