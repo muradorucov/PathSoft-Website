c94df819b2836fbd6c5e6f40aab04113070e0f1e
Murad685 <orucowmurad@gmail.com>
2021-05-23 01:21:39 +0400

    FinalProject team.html Add

--- PROJECT/Backend/main/routes.py
+++ PROJECT/Backend/main/routes.py
@@ -36,6 +36,13 @@ def service_index():
     meniu_names=MeniuName.query.all()
     return render_template("main/services.html", header_contacts=header_contacts, headersocial_icons=headersocial_icons, logos=logos, meniu_names=meniu_names)
 
+@app.route("/team")
+def team_index():
+    header_contacts=HeaderContact.query.all()
+    headersocial_icons=HeaderSocialIcon.query.all()
+    logos=MeniuLogo.query.all()
+    meniu_names=MeniuName.query.all()
+    return render_template("main/team.html", header_contacts=header_contacts, headersocial_icons=headersocial_icons, logos=logos, meniu_names=meniu_names)
 
 
 
